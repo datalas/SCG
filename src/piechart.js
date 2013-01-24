f9620e884aee0b8463f9f6bb6d1ccccff7f13bbc
Datalas <gnupoo@datalas.com>
2013-01-24 15:50:38 +0000

    Fixed flickery pie charts on hover
    The text when it is added can shift focus making the piechart collapse
    and then come back up as the element is removed.

--- src/piechart.js
+++ src/piechart.js
@@ -48,13 +48,18 @@ drawPie: function(){
 
 	/* so, this will be X pixels high, ergo we need to start it at the mid point */
 	var keyHeight = this.options.data.length * 20;
-	this.labelY = cy - ( keyHeight / 2 ) + 10;
+	this.labelY = Math.max( cy - ( keyHeight / 2 ) + 10, this.options.gutter.top );
 	this.labelX = cx + radius + 50;
 	var keyWidth = 0;
+
+	var covers = this.paper.set();
 	
 	this.options.data.each( function( slice, index ){ 
 		var angle = 360 * ( slice / total );
 		var sector = this.sector( cx, cy, radius, starting_angle, starting_angle + angle, { 'fill': this.colours[ index ], 'opacity': 0.6, 'stroke': '#ffffff' } );
+		var sectorCover = this.sector( cx, cy, radius, starting_angle, starting_angle + angle, { 'fill': this.colours[ index ], 'opacity': 0.0 } );
+		covers.push( sectorCover );
+
 		var key = this.addKey( index );
 		var ms = 500;
 		var percentage = parseInt((slice/total) * 100);
@@ -65,26 +70,30 @@ drawPie: function(){
 		
 		var highlight = function(){
 			sector.stop().animate({transform: "s1.1 1.1 " + cx + " " + cy}, ms, "elastic");
+			sectorCover.stop().animate({transform: "s1.1 1.1 " + cx + " " + cy}, ms, "elastic");
 			sector.attr({'opacity': 1 });
 			key.blob.attr({'fill-opacity': 1 });
 			key.blob.scale(2,2);
 			key.text.attr({'font-size': 14});
-			sector._percent = this.paper.text( sector.center.x, sector.center.y, percentage ).attr({'fill': '#ffffff', 'font-size': 30});
+			sector._percent = this.paper.text( sector.center.x, sector.center.y, percentage ).attr({'fill': '#ffffff', 'font-size': 30}).insertBefore( sectorCover );
 		};
 		var removeHighlight = function(){
 			sector.stop().animate({transform: ""}, ms, "elastic");
+			sectorCover.stop().animate({transform: ""}, ms, "elastic");
 			sector.attr({'opacity': 0.6 });
 			key.blob.attr({'fill-opacity': 0.4 });
 			key.blob.scale(0.5,0.5);
 			key.text.attr({'font-size': 12});
 			sector._percent.remove();
 		};
-		sector.hover( highlight, removeHighlight );
+		sectorCover.hover( highlight, removeHighlight );
 		key.blob.hover( highlight, removeHighlight );
 		key.text.hover( highlight, removeHighlight );
 		keyWidth = Math.max( keyWidth, key.width );
 		starting_angle += angle;
 	}, this );
+
+	covers.toFront();
 },
 sector: function(cx, cy, r, startAngle, endAngle, params ) {
 	var rad = Math.PI / 180;
