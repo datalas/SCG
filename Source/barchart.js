4acdf65fa1ccfdd0c0633132924d1ae31fb9b998
datalas <datalas@laserbeak.(none)>
2013-10-08 22:00:41 +0100

    Updated BarCharts to allow for a persistant colouring

--- Source/barchart.js
+++ Source/barchart.js
@@ -24,6 +24,9 @@ provides: [SCGBarchart]
 var SCGBarchart = new Class({
 Implements: [ Events, Options ],
 Extends: SCGChart,
+options: {
+	singleColour: false
+},
 initialize: function( obj, options ){
 	this.parent( obj, options );
 	this.element = $(obj);
@@ -136,7 +139,11 @@ drawBars: function(){
 
 	} else {
 		Array.each( this.options.data, function( value, position ){
-			var bar = this.drawBar( value, this.points.x[position], this.chart.bottom, this.points.xLabels[ position ], this.colours[ position ], this.alphaColours[ position ] );
+			var colour = position;
+			if ( this.options.singleColour ){
+				colour = 0;
+			}
+			var bar = this.drawBar( value, this.points.x[position], this.chart.bottom, this.points.xLabels[ position ], this.colours[ colour ], this.alphaColours[ colour ] );
 			this.bars.push( bar );	
 		}, this );
 	}
